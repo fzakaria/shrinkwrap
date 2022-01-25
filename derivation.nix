e27e59e8538f9d417107f099e5f44d09f3f54558
Farid Zakaria <farid.m.zakaria@gmail.com>
2022-01-24 20:07:01 -0800

    Nixpkg-fmt for derivation.nix

--- derivation.nix
+++ derivation.nix
@@ -1,5 +1,11 @@
-{ stdenv, lib, poetry2nix, python39, poetryOverrides, writeScriptBin
-, makeWrapper }:
+{ stdenv
+, lib
+, poetry2nix
+, python39
+, poetryOverrides
+, writeScriptBin
+, makeWrapper
+}:
 poetry2nix.mkPoetryApplication {
   projectDir = ./.;
   python = python39;
