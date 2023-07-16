95eafdd190dce8694b427774e8ea7e7b88d804af
Fangrui Song <i@maskray.me>
2023-07-15 20:18:11 -0700

    Fix -l native to iterate over lines instead of characters of ld.so output (#8)
    The result of the output of ld.so was from the Command object of the library https://amoffat.github.io/sh/index.html
    The result of the command is the type ResultCommand which behaves very much like a string.
    Looks like when it's iterated it already returns line by line results.
    
    Changes to splitlines() since it's more self-evident to be correct.
    
    Co-authored-by: Farid Zakaria <fmzakari@google.com>

--- shrinkwrap/elf.py
+++ shrinkwrap/elf.py
@@ -40,10 +40,7 @@ class NativeLinkStrategy(LinkStrategy):
         # TODO: Figure out why `--list` and `ldd` produce different outcomes
         # specifically for the interpreter.
         # https://gist.github.com/fzakaria/3dc42a039401598d8e0fdbc57f5e7eae
-        # Additionally: the result of the interpreter(...) call is a
-        # RunningCommand object which supports line iteration.
-        # No need to call splitlines.
-        for line in resolution:
+        for line in resolution.splitlines():
             m = re.match(r"\s*([^ ]+) => ([^ ]+)", line)
             if not m:
                 continue
